305500928556e98589bd5c937fa1c327c96bf003
AwesomePear <matthewsnowprince@gmail.com>
2018-01-23 19:48:29 -0700

    final

--- conways_toroidal_grid.rb
+++ conways_toroidal_grid.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'descriptive_statistics'
 require 'optparse'
 
 params = {
@@ -9,15 +10,19 @@ params = {
   probability_alive: 0.375,
   display_alive: '1',
   display_dead: '0',
+  density: false,
+  grid: false,
 }
 
 parser = OptionParser.new do |op|
-  op.on("-n", "--num-cols <int>", Integer, "the number of cols") {|v| params[:num_cols] = v }
-  op.on("-m", "--num-rows <int>", Integer, "the number of rows") {|v| params[:num_rows] = v }
+  op.on("-n", "--num-cols <#{params[:num_cols]}>", Integer, "the number of cols") {|v| params[:num_cols] = v }
+  op.on("-m", "--num-rows <#{params[:num_rows]}>", Integer, "the number of rows") {|v| params[:num_rows] = v }
   op.on("-i", "--num-iterations <int>", Integer, "the number of iterations") {|v| params[:num_iterations] = v }
-  op.on("-p", "--probability-alive <float>", Float, "probability a cell is alive at start") {|v| params[:probability_alive] = v }
+  op.on("-p", "--probability-alive <#{params[:probability_alive]}>", Float, "probability a cell is alive at start") {|v| params[:probability_alive] = v }
   op.on("-a", "--display-alive <char>", "the character for alive") {|v| params[:display_alive] = v }
   op.on("-d", "--display-dead <char>", "the character for dead") {|v| params[:display_dead] = v }
+  op.on("--density", "display the density") {|v| params[:density] = v }
+  op.on("--grid", "display the grid") {|v| params[:grid] = v }
 end
 parser.parse!
 
@@ -27,6 +32,7 @@ class Grid
   DEAD = 0
   
   attr_accessor :rows
+  attr_accessor :all_densities
   
   def initialize_board(num_rows, num_cols, probability_alive)
     num_rows.times.with_object([]) do |row_index, board|
@@ -70,27 +76,47 @@ class Grid
     end
   end
   
+  def calculate_density
+    all_cells = every_cell
+    all_cells.sum.to_f / all_cells.size
+  end
+  
   def iterate(num)
     (num+1).times do |iteration|
-      puts "-" * [@rows.first.size, 14].max
-      puts "iteration #{iteration}"
-      puts self.to_s
-      puts "-" * [@rows.first.size, 14].max
+      #puts "-" * [@rows.first.size, 14].max
+      #puts "iteration #{iteration}"
+      #puts to_s
+      density = calculate_density
+      @all_densities << density
+      if @params[:density]
+        puts density
+      end
+      if @params[:grid]
+        puts "-" * [@rows.first.size, 14].max
+        puts to_s
+        puts "-" * [@rows.first.size, 14].max
+      end
+      #puts "#{density * 100}%"
+      #puts "-" * [@rows.first.size, 14].max
       @rows = calculate_new_board
     end
   end
   
-  def initialize(num_rows, num_cols, probability_alive, display_alive, display_dead)
-    @rows = initialize_board(num_rows, num_cols, probability_alive)
-    @display_alive = display_alive
-    @display_dead = display_dead
+  def avg_density
+    @all_densities.sum.to_f / @all_densities.size
+  end
+  
+  def initialize(params)
+    @params = params
+    @rows = initialize_board(@params[:num_rows], @params[:num_cols], @params[:probability_alive])
+    @all_densities = []
   end
   
   def to_s
     response = ''
     @rows.each do |row|
       row.each do |cell|
-        response << (cell == 1 ? @display_alive : @display_dead)
+        response << (cell == 1 ? @params[:display_alive] : @params[:display_dead])
       end
       response << "\n"
     end
@@ -103,13 +129,15 @@ class Grid
     
 end
 
-grid = Grid.new(
-  params[:num_rows],
-  params[:num_cols],
-  params[:probability_alive],
-  params[:display_alive],
-  params[:display_dead],
-)
-grid.iterate(params[:num_iterations])
+num_trials = 20
+avg_densities = num_trials.times.map do |n|
+  puts "trial #{n}"
+  grid = Grid.new(params)
+  grid.iterate(params[:num_iterations])
+  grid.avg_density
+end
+require 'yaml'
+puts avg_densities.descriptive_statistics.to_yaml
+
 
 
